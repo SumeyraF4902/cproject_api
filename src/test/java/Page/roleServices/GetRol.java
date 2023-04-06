8274f722489a93889f0d2d70a60a28fc46ecb684
Sumeyra <sumeyra81.oz@gmail.com>
2023-04-06 14:04:57 +0300

    calisma

---
+++ src/test/java/Page/roleServices/GetRol.java
@@ -0,0 +1,22 @@
+package Page.roleServices;
+
+import BaseUrl.BaseURL;
+import io.restassured.response.Response;
+import org.junit.Test;
+import resources.Token;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GetRol extends BaseURL {
+
+    }
+
+
+
+
+
